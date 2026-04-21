a3f013f16d2b244cfe3f4304b5876b7b99946147
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2026-04-21 20:37:01 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +13,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export const metadata: Metadata = {
   title: "Create Next App",
